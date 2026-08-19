7b5d0d99ee94c3d064557b17fca130001d2428e9
Gilbert Luis <gilbertluis97@gmail.com>
2026-08-19 00:10:04 -0300

    add react-router-dom for routing and set up initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+
 import './App.css';
+import { router } from './router';
 
 function App() {
-  return (
-    <>
-      <h1>TestR eact</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
-
 export default App;
