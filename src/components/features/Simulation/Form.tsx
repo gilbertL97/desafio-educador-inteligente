9e5212a2f4cca34cd894e5b8b3d7de4c061191ba
Gilbert Luis <gilbertluis97@gmail.com>
2026-08-20 03:29:28 -0300

    add FormStep and Input components for enhanced form functionality in SimulationForm

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,22 @@
+import { PiggyBank } from 'lucide-react';
+
+import { FormStep } from './FormStep';
 import { StepProgress } from './Progres';
 
 export function SimulationForm() {
   return (
     <>
       <StepProgress currentStep={3} totalSteps={3} />
+      <FormStep
+        icon={PiggyBank}
+        title="Renda  Mensal Bruta"
+        question="Quanto e depositado na sua conta todo mes (somando todas as fontes)"
+        inputProps={{
+          type: 'text',
+          placeholder: '5.000.00',
+          prefix: 'R$ ',
+        }}
+      />
     </>
   );
 }
