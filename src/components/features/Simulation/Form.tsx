49f7aa9a8f7df8b81604d9137cedb43772cc4dda
Gilbert Luis <gilbertluis97@gmail.com>
2026-08-20 02:55:10 -0300

    add StepProgress component for progress indication in SimulationForm

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,7 +1,9 @@
+import { StepProgress } from './Progres';
+
 export function SimulationForm() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <h1 className="text-4xl font-bold mb-8">Formulário de Simulação</h1>
-    </div>
+    <>
+      <StepProgress currentStep={3} totalSteps={3} />
+    </>
   );
 }
